ad7f6ab8bd6c660bf8269ac16ea76c17acf2be10
jigyasagour <jigyashagour@gmail.com>
2026-05-27 12:10:05 +0530

    Solve Contains Duplicate - HashSet approach

---
+++ arrays/contains_duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        arr = set()
+        for i in nums:
+            if i in arr:
+                return True 
+            arr.add(i)
+        return False        
